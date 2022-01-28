b408bd68996a0440ceee8ae686a8fe945603e317
hwakk <napia1014@gmail.com>
2022-01-29 00:00:47 +0900

    first commit

---
+++ src/Pages/Profile.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+function Profile() {
+  let navigate = useNavigate();
+  let { username } = useParams();
+  return (
+    <div>
+      Profile for {username}
+      <button
+        onClick={() => {
+          navigate("/about");
+        }}
+      >
+        Change to about page
+      </button>
+    </div>
+  );
+}
+
+export default Profile;
